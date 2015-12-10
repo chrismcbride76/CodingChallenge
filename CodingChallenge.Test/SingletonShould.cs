4081fc1f00d35b11965c3a77efb9fd03182e6329
Chris McBride <chrismcbride76@gmail.com>
2015-12-09 18:19:55 -0700

    Add thread safe singleton

---
+++ CodingChallenge.Test/SingletonShould.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingChallenge.Test
+{
+    [TestClass]
+    public class SingletonShould
+    {
+        [TestMethod]
+        public void HaveSingleInstance()
+        {
+            Singleton ref1 = Singleton.Instance;
+            Singleton ref2 = Singleton.Instance;
+
+            Assert.IsTrue(ReferenceEquals(ref1, ref2));
+        }
+    }
+}
---
+++ CodingChallenge/Singleton.cs
@@ -0,0 +1,17 @@
+﻿namespace CodingChallenge
+{
+public sealed class Singleton
+{
+   private static readonly Singleton instance = new Singleton();
+   
+   private Singleton(){}
+
+   public static Singleton Instance
+   {
+      get 
+      {
+         return instance; 
+      }
+   }
+}
+}
